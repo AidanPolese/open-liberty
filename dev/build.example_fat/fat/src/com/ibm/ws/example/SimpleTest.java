31b05aaeaf7d9b1b63ff135b50778d36403672a4
Andrew Guibert <aguibert@us.ibm.com>
2017-08-04 10:49:29 -0500

    Move standard FAT build steps to common gradle build file

--- dev/build.example_fat/fat/src/com/ibm/ws/example/SimpleTest.java
+++ dev/build.example_fat/fat/src/com/ibm/ws/example/SimpleTest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package com.ibm.ws.example;
 
 import java.io.File;
@@ -20,15 +30,13 @@ import componenttest.topology.utils.FATServletClient;
 
 /**
  * Example Shrinkwrap FAT project:
- * <li> Uses minimal ant scritping, so that the bucket can be run using the green
- * 'play' button in an eclipse IDE or with the traditional build-test.xml via ant.
  * <li> Application packaging is done in the @BeforeClass, instead of ant scripting.
  * <li> Injects servers via @Server annotation. Annotation value corresponds to the
  * server directory name in 'publish/servers/%annotation_value%' where ports get
  * assigned to the LibertyServer instance when the 'testports.properties' does not
  * get used.
  * <li> Specifies an @RunWith(FATRunner.class) annotation. Traditionally this has been
- * added to bytecode automatically by ant, and is a small downside to factoring out ant.
+ * added to bytecode automatically by ant.
  * <li> Uses the @TestServlet annotation to define test servlets. Notice that no @Test
  * methods are defined in this class. All of the @Test methods are defined on the test
  * servlet referenced by the annotation, and will be run whenever this test class runs.
