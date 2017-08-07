4e797be76ac88697712b2ec4948f69255311da90
Nathan Rauh <nathan.rauh@us.ibm.com>
2017-08-07 11:26:37 -0500

    Issue #6793 add copyrights for policy executor test

--- dev/com.ibm.ws.threading_policy_fat/test-bundles/testpolicyexecutor/src/test/policyexecutor/bundle_fat/PolicyExecutorProviderFactory.java
+++ dev/com.ibm.ws.threading_policy_fat/test-bundles/testpolicyexecutor/src/test/policyexecutor/bundle_fat/PolicyExecutorProviderFactory.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package test.policyexecutor.bundle_fat;
 
 import org.osgi.service.component.annotations.Component;
@@ -10,9 +20,10 @@ import com.ibm.wsspi.resource.ResourceFactory;
 import com.ibm.wsspi.resource.ResourceInfo;
 
 /**
- *
+ * A ResourceFactory implementation that makes PolicyExecutorProvider available to applications via JNDI for convenience of writing tests.
  */
-@Component(name = "TestPolicyExecutorProvider", configurationPolicy = ConfigurationPolicy.IGNORE, immediate = true, property = { "jndiName=test/TestPolicyExecutorProvider" })
+@Component(name = "TestPolicyExecutorProvider", configurationPolicy = ConfigurationPolicy.IGNORE,
+           property = { "jndiName=test/TestPolicyExecutorProvider", "creates.objectClass=com.ibm.ws.threading.PolicyExecutorProvider" })
 public class PolicyExecutorProviderFactory implements ResourceFactory {
 
     @Reference
