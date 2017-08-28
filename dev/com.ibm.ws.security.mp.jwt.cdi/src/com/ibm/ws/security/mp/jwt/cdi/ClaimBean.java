defb08399e39047eace0485daeb020b8032d1a65
Teddy J. Torres <ttorres@us.ibm.com>
2017-08-28 15:36:40 -0400

    Initial MicroProfile JWT CDI support for issue #511

---
+++ dev/com.ibm.ws.security.mp.jwt.cdi/src/com/ibm/ws/security/mp/jwt/cdi/ClaimBean.java
@@ -0,0 +1,381 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.security.mp.jwt.cdi;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.CDI;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.PassivationCapable;
+
+import org.eclipse.microprofile.jwt.Claim;
+import org.eclipse.microprofile.jwt.ClaimValue;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import com.ibm.websphere.ras.Tr;
+import com.ibm.websphere.ras.TraceComponent;
+
+/**
+ *
+ */
+public class ClaimBean<T> implements Bean<T>, PassivationCapable {
+
+    private static final TraceComponent tc = Tr.register(ClaimBean.class);
+
+    private final Class<T> beanClass;
+    private Type beanType;
+    private Claim claim;
+    private Set<Type> types;
+    private Set<Annotation> qualifiers;
+    private String name;
+    private String id;
+
+    public ClaimBean(BeanManager beanManager, Class<T> beanClass, Claim claim) {
+        this(beanManager, beanClass, beanClass, claim);
+    }
+
+    public ClaimBean(BeanManager beanManager, Type beanType, Class<T> beanClass, Claim claim) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "<init>", beanManager, beanType, beanClass, claim);
+        }
+        this.beanType = beanType;
+        this.beanClass = beanClass;
+        this.claim = claim;
+        this.types = new HashSet<Type>();
+        types.add(beanType);
+        this.qualifiers = new HashSet<Annotation>();
+        this.qualifiers.add(claim);
+        this.name = this.getClass().getName() + "[" + claim + "," + beanType + "]";
+        this.id = beanManager.hashCode() + "#" + this.name;
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "<init>", this);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @SuppressWarnings("unchecked")
+    @Override
+    public T create(CreationalContext<T> creationalContext) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "create", creationalContext);
+        }
+
+        T instance = null;
+        CDI<Object> cdi = CDI.current();
+        BeanManager beanManager = cdi.getBeanManager();
+
+        if (beanType instanceof ParameterizedType) {
+            instance = createClaimValueForParameterizedType(creationalContext, beanManager);
+        } else if (beanType instanceof Class) {
+            instance = createClaimValueForClassType();
+        } else {
+            throw new IllegalArgumentException(Tr.formatMessage(tc, "unable.to.determine.injection.type.CWMCG5001E", beanType));
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "create", instance);
+        }
+        return instance;
+    }
+
+    /**
+     * @param creationalContext
+     * @param beanManager
+     * @return
+     */
+    private T createClaimValueForParameterizedType(CreationalContext<T> creationalContext, BeanManager beanManager) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "createClaimValueForParameterizedType", creationalContext, beanManager);
+        }
+
+        T instance = null;
+        ParameterizedType parameterizedType = (ParameterizedType) beanType;
+        Type actualReturnType = parameterizedType.getActualTypeArguments()[0];
+        if (actualReturnType instanceof ParameterizedType) {
+            actualReturnType = ((ParameterizedType) actualReturnType).getRawType();
+        }
+
+        boolean isOptional = Optional.class.isAssignableFrom((Class<?>) actualReturnType);
+        Class<?> returnClass = (Class<?>) actualReturnType;
+        instance = getClaimValue(returnClass, isOptional);
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "createClaimValueForParameterizedType", instance);
+        }
+        return instance;
+    }
+
+    /**
+     * @param <U>
+     * @param injectionPoint
+     * @param returnClass
+     * @param aType
+     * @return
+     */
+    private <U> T getClaimValue(Class<U> returnClass, boolean isOptional) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "getClaimValue", returnClass, isOptional);
+        }
+
+        T instance = null;
+        final String claimName = getClaimName();
+        instance = (T) new ClaimValue() {
+
+            @Override
+            public String getName() {
+                return claimName;
+            }
+
+            @Override
+            public U getValue() {
+                U value = null;
+                JsonWebToken jsonWebToken = null;
+                Instance<JsonWebToken> jsonWebTokenInstance = CDI.current().select(JsonWebToken.class);
+
+                if (jsonWebTokenInstance != null && jsonWebTokenInstance.isAmbiguous() == false && jsonWebTokenInstance.isUnsatisfied() == false) {
+                    jsonWebToken = jsonWebTokenInstance.get();
+
+                    if (isOptional) {
+                        value = (U) jsonWebToken.claim(claimName);
+                    } else {
+                        value = (U) jsonWebToken.getClaim(claimName);
+                    }
+                }
+
+                return value;
+            }
+        };
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "getClaimValue", instance);
+        }
+        return instance;
+    }
+
+    /**
+     * @param instance
+     * @return
+     */
+    private T createClaimValueForClassType() {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "createClaimValueForClassType");
+        }
+
+        T instance = null;
+        Class<T> ipClass = (Class<T>) beanType;
+        String claimName = getClaimName();
+        if (ClaimValue.class.equals(ipClass)) {
+            instance = (T) new ClaimValue() {
+
+                @Override
+                public String getName() {
+                    return claimName;
+                }
+
+                @Override
+                public Object getValue() {
+                    Object value = null;
+                    JsonWebToken jsonWebToken = null;
+                    Instance<JsonWebToken> jsonWebTokenInstance = CDI.current().select(JsonWebToken.class);
+
+                    if (jsonWebTokenInstance != null && jsonWebTokenInstance.isAmbiguous() == false && jsonWebTokenInstance.isUnsatisfied() == false) {
+                        jsonWebToken = jsonWebTokenInstance.get();
+                        value = jsonWebToken.getClaim(claimName);
+                    }
+
+                    return value;
+                }
+            };
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "createClaimValueForClassType", instance);
+        }
+        return instance;
+    }
+
+    private String getClaimName() {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "getClaimName");
+        }
+
+        String claimName = claim.value();
+        if (claimName == null || claimName.trim().isEmpty()) {
+            claimName = claim.standard().name();
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "getClaimName", claimName);
+        }
+        return claimName;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void destroy(T instance, CreationalContext<T> creationalContext) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "destroy", instance, creationalContext);
+        }
+
+        creationalContext.release();
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "destroy");
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Class<?> getBeanClass() {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "getBeanClass");
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "getBeanClass", beanClass);
+        }
+        return beanClass;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean isNullable() {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "isNullable");
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "isNullable", true);
+        }
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Class<? extends Annotation> getScope() {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "getScope");
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "getScope", RequestScoped.class);
+        }
+        return RequestScoped.class;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Set<Type> getTypes() {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "getTypes");
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "getTypes", types);
+        }
+        return types;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Set<Annotation> getQualifiers() {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "getQualifiers");
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "getQualifiers", qualifiers);
+        }
+        return qualifiers;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getName() {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "getName");
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "getName", name);
+        }
+        return name;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Set<Class<? extends Annotation>> getStereotypes() {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "getStereotypes");
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "getStereotypes", Collections.emptySet());
+        }
+        return Collections.emptySet();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean isAlternative() {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "isAlternative");
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "isAlternative", false);
+        }
+        return false;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Set<InjectionPoint> getInjectionPoints() {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "getInjectionPoints");
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "getInjectionPoints", Collections.emptySet());
+        }
+        return Collections.emptySet();
+    }
+
+    @Override
+    public String getId() {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "getId");
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "getId", id);
+        }
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Bean for " + this.types + " with Qualifiers " + this.qualifiers;
+    }
+
+}
---
+++ dev/com.ibm.ws.security.mp.jwt.cdi/src/com/ibm/ws/security/mp/jwt/cdi/JwtCDIExtension.java
@@ -0,0 +1,246 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.security.mp.jwt.cdi;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AfterBeanDiscovery;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.ProcessInjectionTarget;
+import javax.inject.Provider;
+
+import org.eclipse.microprofile.jwt.Claim;
+import org.osgi.service.component.annotations.Component;
+
+import com.ibm.websphere.ras.Tr;
+import com.ibm.websphere.ras.TraceComponent;
+import com.ibm.ws.cdi.extension.WebSphereCDIExtension;
+
+/**
+ *
+ */
+@Component(service = WebSphereCDIExtension.class, property = { "api.classes=org.eclipse.microprofile.jwt.Claim;org.eclipse.microprofile.jwt.Claims;org.eclipse.microprofile.jwt.ClaimValue" }, immediate = true)
+public class JwtCDIExtension implements Extension, WebSphereCDIExtension {
+
+    private static final TraceComponent tc = Tr.register(JwtCDIExtension.class);
+
+    private final Map<Claim, Set<Type>> injectionTypes = new HashMap<Claim, Set<Type>>();
+
+    public void processInjectionTarget(@Observes ProcessInjectionTarget<?> pit) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "processInjectionTarget", pit);
+        }
+
+        Class<?> targetClass = pit.getAnnotatedType().getJavaClass();
+        ClassLoader classLoader = targetClass.getClassLoader();
+
+        for (InjectionPoint injectionPoint : pit.getInjectionTarget().getInjectionPoints()) {
+            Claim claim = getClaimAnnotation(injectionPoint);
+            if (claim != null) {
+                Type type = injectionPoint.getType();
+                Throwable configException = null;
+                if (type instanceof ParameterizedType) {
+                    ParameterizedType pType = (ParameterizedType) type;
+                    configException = processParameterizedType(injectionPoint, pType, classLoader, claim);
+                } else {
+                    configException = validateInjectionPoint(injectionPoint, type, type, classLoader, false, claim);
+                }
+                if (configException != null) {
+                    Tr.error(tc, "unable.to.resolve.injection.point.CWMCG5003E", injectionPoint, configException); // TODO: Update message key
+                }
+            }
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "processInjectionTarget");
+        }
+    }
+
+    private Claim getClaimAnnotation(InjectionPoint injectionPoint) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "getClaimAnnotation", injectionPoint);
+        }
+        Claim claim = null;
+
+        Set<Annotation> qualifiers = injectionPoint.getQualifiers();
+        if (qualifiers != null) {
+            //find the qualifier
+            for (Annotation qualifier : qualifiers) {
+                if (qualifier.annotationType().equals(Claim.class)) {
+                    claim = (Claim) qualifier;
+                    break;
+                }
+            }
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "getClaimAnnotation", claim);
+        }
+        return claim;
+    }
+
+    public void afterBeanDiscovery(@Observes AfterBeanDiscovery abd, BeanManager beanManager) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "afterBeanDiscovery", abd, beanManager);
+        }
+
+        for (Entry<Claim, Set<Type>> entrySet : injectionTypes.entrySet()) {
+            Claim claim = entrySet.getKey();
+            for (Type type : entrySet.getValue()) {
+                try {
+                    if (type instanceof TypeVariable) {
+                        TypeVariable<?> typeVar = (TypeVariable<?>) type;
+                        Type[] bounds = typeVar.getBounds();
+                        for (Type bound : bounds) {
+                            addClaimBean(abd, beanManager, bound, claim);
+                        }
+                    } else {
+                        addClaimBean(abd, beanManager, type, claim);
+                    }
+                } catch (ClaimTypeException e) {
+                    abd.addDefinitionError(e);
+                }
+            }
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "afterBeanDiscovery");
+        }
+    }
+
+    private Throwable processParameterizedType(InjectionPoint injectionPoint, ParameterizedType injectionType, ClassLoader classLoader, Claim claim) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "processParameterizedType", injectionPoint, injectionType, classLoader, claim);
+        }
+
+        Throwable configException = null;
+        Type rawType = injectionType.getRawType();
+        Type returnType = injectionType.getActualTypeArguments()[0];
+
+        if (Provider.class.isAssignableFrom((Class<?>) rawType)) {
+            configException = validateInjectionPoint(injectionPoint, returnType, returnType, classLoader, false, claim);
+        } else if (Optional.class.isAssignableFrom((Class<?>) rawType)) {
+            Type[] aTypes = injectionType.getActualTypeArguments();
+            returnType = aTypes[0];
+            configException = validateInjectionPoint(injectionPoint, returnType, injectionType, classLoader, true, claim);
+        } else {
+            if (returnType instanceof ParameterizedType) {
+                returnType = ((ParameterizedType) returnType).getRawType();
+            }
+            configException = validateInjectionPoint(injectionPoint, returnType, injectionType, classLoader, false, claim);
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "processParameterizedType", configException);
+        }
+        return configException;
+    }
+
+    private Throwable validateInjectionPoint(InjectionPoint injectionPoint, Type conversionType, Type injectionType, ClassLoader classLoader, boolean optional, Claim claim) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "validateInjectionPoint", conversionType, injectionType, classLoader, optional, claim);
+        }
+
+        Throwable configException = null;
+        Type rawInjectionType = injectionType;
+
+        if (injectionType instanceof ParameterizedType) {
+            rawInjectionType = ((ParameterizedType) injectionType).getRawType();
+        }
+
+        // TODO: Validate type
+//        if (ClaimValue.class.isAssignableFrom((Class<?>) rawInjectionType) || Provider.class.isAssignableFrom((Class<?>) rawInjectionType)) {
+        try {
+            Set<Type> injectionTypesForQualifier = injectionTypes.get(claim);
+            if (injectionTypesForQualifier == null) {
+                injectionTypesForQualifier = new HashSet<Type>();
+                injectionTypes.put(claim, injectionTypesForQualifier);
+            }
+            injectionTypesForQualifier.add(injectionType);
+        } catch (Throwable e) {
+            configException = e;
+        }
+//        } else {
+//            configException = new ClaimTypeException(Tr.formatMessage(tc, "unable.to.determine.injection.type.CWMCG5001E", injectionType));
+//        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "validateInjectionPoint", configException);
+        }
+        return configException;
+    }
+
+    private void addClaimBean(AfterBeanDiscovery abd, BeanManager beanManager, Type type, Claim claim) throws ClaimTypeException {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "addClaimBean", abd, beanManager, type, claim);
+        }
+
+        if (type instanceof Class) {
+            Class<?> clazz = (Class<?>) type;
+            if (!clazz.isPrimitive()) {
+                addClaimBean(abd, beanManager, type, clazz, claim);
+            }
+        } else if (type instanceof ParameterizedType) {
+            ParameterizedType pType = (ParameterizedType) type;
+            addClaimBean(abd, beanManager, pType, claim);
+        } else {
+            throw new ClaimTypeException(Tr.formatMessage(tc, "unable.to.determine.injection.type.CWMCG5001E", type));
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "addClaimBean");
+        }
+    }
+
+    private void addClaimBean(AfterBeanDiscovery abd, BeanManager beanManager, ParameterizedType type, Claim claim) throws ClaimTypeException {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "addClaimBean", abd, beanManager, type, claim);
+        }
+
+        Type rawInjectionType = type.getRawType();
+        if (rawInjectionType instanceof Class) {
+            Class<?> clazz = (Class<?>) rawInjectionType;
+            addClaimBean(abd, beanManager, type, clazz, claim);
+        } else {
+            throw new ClaimTypeException(Tr.formatMessage(tc, "unable.to.determine.injection.type.CWMCG5001E", type));
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "addClaimBean");
+        }
+    }
+
+    private <T> void addClaimBean(AfterBeanDiscovery abd, BeanManager beanManager, Type beanType, Class<T> clazz, Claim claim) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "addClaimBean", abd, beanManager, beanType, clazz, claim);
+        }
+
+        ClaimBean<T> converterBean = new ClaimBean<T>(beanManager, beanType, clazz, claim);
+        abd.addBean(converterBean);
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "addClaimBean");
+        }
+    }
+
+}
