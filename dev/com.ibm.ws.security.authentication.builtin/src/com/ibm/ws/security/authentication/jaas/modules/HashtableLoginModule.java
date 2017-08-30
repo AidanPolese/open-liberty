5295d71a9bd9e65c46fb3d16207ffb808d0057a5
Andy Naumann <naumann@us.ibm.com>
2017-08-30 13:32:57 -0500

    Revert PR699 "create microprofile jwt proxy project"
    This reverts commit 026fd0b49e560718173ef455f5dc2f5792795113.

--- dev/com.ibm.ws.security.authentication.builtin/src/com/ibm/ws/security/authentication/jaas/modules/HashtableLoginModule.java
+++ dev/com.ibm.ws.security.authentication.builtin/src/com/ibm/ws/security/authentication/jaas/modules/HashtableLoginModule.java
@@ -35,7 +35,6 @@ import com.ibm.ws.security.authentication.AuthenticationService;
 import com.ibm.ws.security.authentication.internal.jaas.modules.ServerCommonLoginModule;
 import com.ibm.ws.security.authentication.principals.WSPrincipal;
 import com.ibm.ws.security.authentication.utility.SubjectHelper;
-import com.ibm.ws.security.mp.jwt.proxy.MpJwtHelper;
 import com.ibm.ws.security.registry.EntryNotFoundException;
 import com.ibm.ws.security.registry.RegistryException;
 import com.ibm.ws.security.registry.UserRegistry;
@@ -55,12 +54,11 @@ public class HashtableLoginModule extends ServerCommonLoginModule implements Log
     private String customRealm = null;
 
     private final String[] hashtableLoginProperties = { AttributeNameConstants.WSCREDENTIAL_UNIQUEID,
-                                                        AttributeNameConstants.WSCREDENTIAL_USERID,
-                                                        AttributeNameConstants.WSCREDENTIAL_SECURITYNAME,
-                                                        AttributeNameConstants.WSCREDENTIAL_REALM,
-                                                        AttributeNameConstants.WSCREDENTIAL_CACHE_KEY,
-                                                        AuthenticationConstants.INTERNAL_ASSERTION_KEY,
-                                                        AuthenticationConstants.INTERNAL_JSON_WEB_TOKEN };
+                                                       AttributeNameConstants.WSCREDENTIAL_USERID,
+                                                       AttributeNameConstants.WSCREDENTIAL_SECURITYNAME,
+                                                       AttributeNameConstants.WSCREDENTIAL_REALM,
+                                                       AttributeNameConstants.WSCREDENTIAL_CACHE_KEY,
+                                                       AuthenticationConstants.INTERNAL_ASSERTION_KEY };
 
     private boolean uniquedIdAndSecurityNameLogin = false;
     private boolean useIdAndPasswordLogin = false;
@@ -256,7 +254,6 @@ public class HashtableLoginModule extends ServerCommonLoginModule implements Log
             }
             setPrincipalAndCredentials(temporarySubject, username, null, accessId, WSPrincipal.AUTH_METHOD_HASH_TABLE);
             addJaspicPrincipal(temporarySubject);
-            addJsonWebToken(temporarySubject);
             updateSharedState();
         } catch (Exception e) {
             throw new AuthenticationException(e.getLocalizedMessage(), e);
@@ -293,7 +290,7 @@ public class HashtableLoginModule extends ServerCommonLoginModule implements Log
     /**
      * If a JASPIC provider supplied a Principal via CallerPrincipalCallback
      * then put the Principal in the Principals set and in the WSCredential
-     *
+     * 
      * @param subject
      */
     private void addJaspicPrincipal(Subject subject) throws Exception {
@@ -309,10 +306,6 @@ public class HashtableLoginModule extends ServerCommonLoginModule implements Log
             }
             subject.getPrincipals().add(jaspiPrincipal);
         }
-    }
-
-    private void addJsonWebToken(Subject subject) throws Exception {
-        MpJwtHelper.addJsonWebToken(subject, customProperties, AuthenticationConstants.INTERNAL_JSON_WEB_TOKEN);
     }
 
     private void setUpTemporarySubject() throws Exception {
@@ -393,7 +386,6 @@ public class HashtableLoginModule extends ServerCommonLoginModule implements Log
         Set<Object> publicCredentials = subject.getPublicCredentials();
         publicCredentials.remove(props);
         props.remove(AuthenticationConstants.INTERNAL_ASSERTION_KEY);
-        props.remove(AuthenticationConstants.INTERNAL_JSON_WEB_TOKEN);
         props.remove(AttributeNameConstants.WSCREDENTIAL_USERID);
         if (!props.isEmpty()) {
             publicCredentials.add(props);
--- dev/com.ibm.ws.security.mp.jwt.proxy/src/com/ibm/ws/security/mp/jwt/proxy/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * @version 1.0.0
- */
-@org.osgi.annotation.versioning.Version("1.0.0")
-package com.ibm.ws.security.mp.jwt.proxy;
